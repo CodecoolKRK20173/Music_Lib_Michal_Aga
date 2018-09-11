4aa3a5a6cf9e55d9cb68964cc21951b457f365d4
Michał Prądzyński <pradzynski.michal@gmail.com>
2018-09-11 14:57:29 +0200

    inputs.py update

--- inputs.py
+++ inputs.py
@@ -1,23 +1,21 @@
 def menu():
-    print("************MENU MUSIC**************")
-    #time.sleep(1)
-    print()
+    print("************MENU MUSIC**************\n")
 
     choice = input("""
-                      A: Export/Imports
-                      B: Displays Albums
-                      C: Search / Handling user
+                      A: Import/Export
+                      B: Display Albums
+                      C: Album Search
                       D: Reports
-                      Q: Quit/Log Out
+                      Q: Quit
 
                       Please enter your choice: """)
 
     if choice == "A" or choice =="a":
-        enterstudentdetails()
+        import_albums() #function from file_handler.py
     elif choice == "B" or choice =="b":
         viewstudentdetails()
     elif choice == "C" or choice =="c":
-        searchbyid()
+        search_menu()
     elif choice=="D" or choice=="d":
         producereports()
     elif choice=="Q" or choice=="q":
@@ -26,3 +24,4 @@ def menu():
         print("You must only select either A,B,C, or D.")
         print("Please try again")
         menu()
+
