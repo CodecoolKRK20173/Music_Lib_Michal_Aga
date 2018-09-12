f261ceaf1c9c56312e0d57a0a7fcf3d3f0c8492d
Michał Prądzyński <pradzynski.michal@gmail.com>
2018-09-12 11:42:49 +0200

    year_search added

--- inputs.py
+++ inputs.py
@@ -57,5 +57,15 @@ def search_menu():
         print("Please enter a number from 1 to 7.")
         search_menu()
 
+def year_search(input_albums):
+    year_range = input('Enter a year range from which you want to find albums (yyyy-yyyy): ')
+    year_range = year_range.split('-')
+    year_range = [int(year) for year in year_range]
+    print(year_range)
+    for album in input_albums:
+        if int(album[2]) in range(year_range[0],year_range[1]+1):
+            print('| {} | {} | {} | {} | {} |'.format(album[0], album[1], album[2], album[3], album[4]))
+
+
 album_list = [['Pink Floyd', 'The Dark Side Of The Moon', '1973', 'progressive rock', '43:00'], ['Britney Spears', 'Baby One More Time', '1999', 'pop', '42:20'], ['The Beatles', 'Revolver', '1966', 'rock', '34:43'], ['Deep Purple', 'Machine Head', '1972', 'hard rock', '37:25'], ['Old Timers', 'Old Time', '966', 'ancient', '123:45'], ['Pink Floyd', 'Wish You Were Here', '1975', 'progressive rock', '44:28'], ['Boston', 'Boston', '1976', 'hard rock', '37:41'], ['Monika Brodka', 'Granada', '2010', 'pop', '37:42'], ['David Bowie', 'Low', '1977', 'rock', '38:26'], ['rock', 'rock', '966', 'pop', '13:37'], ['Massive Attack', 'Blue Lines', '1991', 'hip hop', '45:02']]
-menu()
+year_search(album_list)
