82d26f723237f6022f4640d5bea5044e90bfb683
Michał Prądzyński <pradzynski.michal@gmail.com>
2018-09-12 10:26:19 +0200

    inputs exhanced with album_search function

--- inputs.py
+++ inputs.py
@@ -25,3 +25,33 @@ def menu():
         print("Please try again")
         menu()
 
+def search_menu():
+    search_choice = input("""
+                      1: Find albums by genre
+                      2: Find albums by year range
+                      3: Show the shortest album
+                      4: Show the longest album
+                      5: Find albums created by given artist
+                      6: Find album by album name
+                      7: Quit to main menu
+
+                      Please enter your choice: """)    
+
+    if search_choice == '1':
+        genre_search()
+    elif search_choice == '2':
+        year_range()
+    elif search_choice == '3':
+        show_shortest_longest_album(shortest)
+    elif search_choice == '4':
+        show_shortest_longest_album(longest)
+    elif search_choice == '5':
+        artist_search()
+    elif search_choice == '6':
+        album_name_search()
+    elif search_choice == '7':
+        menu()
+    else:
+        print("Please enter a number from 1 to 7.")
+        search_menu()
+
