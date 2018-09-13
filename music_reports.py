a445d5320498d2406d19cafdca25940c67c857c7
Michał Prądzyński <pradzynski.michal@gmail.com>
2018-09-13 14:45:09 +0200

    everything working except oldest/newest

--- music_reports.py
+++ music_reports.py
@@ -1,85 +1,50 @@
-import file_handling
-import main # pytanie bo tutaj chyba będziemy mieć menu.py ? nie ?
+import inputs
+import display
+import os
 
-def get_albums_by_genre(albums, genre):
-    genre_on_list = []
-    for i in range(len(albums)):
-        genre_on_list.append(albums[i][3])
+def display_raport(album_list):
+    os.system("clear")
+    print("Music Library Raport: ")
+    print("Number of artists: {}".format(count_artists(album_list)))
+    print("Number of albums: {}".format(count_albums(album_list)))
+    print("Number of genres: {}".format(count_genres(album_list)))
+    print("\nThe longest album: ") 
+    inputs.show_shortest_longest_album(album_list, 'longest')
+    print("\nThe shortest album: ")
+    inputs.show_shortest_longest_album(album_list, 'shortest')
+    print("\nThe oldest album: ")
+    print("\nThe newest album: ")
+    
+    print("\nAll imported albums: ")
+    display.display_table_header()
+    for album in album_list:
+        display.display_album_to_print(album)
 
-    if genre in genre_on_list:
-        albums_by_genre = []
-        for i in range(len(albums)):
-            if albums[i][3] == genre:
-                albums_by_genre.append(albums[i])
-    else:
-        raise ValueError("Wrong genre type")
+def count_artists(input_list):
+    artists_number = len(input_list)+1
+    artist_to_compare = input_list[0][0]
+    for line in input_list:
+        if artist_to_compare == line[0]:
+            artists_number -= 1
+    return artists_number
 
-    return albums_by_genre
+def count_albums(input_list):
+    album_number = len(input_list)+1
+    album_to_compare = input_list[0][1]
+    for line in input_list:
+        if album_to_compare == line[1]:
+            album_number -= 1
+    return album_number
 
-
-def get_genre_stats(albums):
-    genre_stats = {}
-    for i in range(len(albums)):
-        if albums[i][3] in genre_stats:
-            genre_stats[albums[i][3]] += 1
-        else:
-            genre_stats[albums[i][3]] = 1
-    return genre_stats
-
-def get_last_oldest(albums):
-
-    earliest = albums[0][2]
-    last_oldest = albums[0]
-
-    for i in range(len(albums)-1):
-        if int(earliest) >= int(albums[i+1][2]):
-            earliest = albums[i+1][2]
-            last_oldest = albums[i+1]
-
-
-    return last_oldest
-
-
-def get_last_oldest_of_genre(albums, genre):
-    """
-    Get last album with earliest release year in given genre
-    """
-    albums_list_by_genre = get_albums_by_genre(albums, genre)
-    last_oldest_of_genre = get_last_oldest(albums_list_by_genre)
-
-    return last_oldest_of_genre
+def count_genres(input_list):
+    genres_number = len(input_list)+1
+    genres_to_compare = input_list[0][3]
+    for line in input_list:
+        if genres_to_compare == line[3]:
+            genres_number -= 1
+    return genres_number
 
 
-"""albums = file_handling.import_data(filename='albums_data.txt')
-genre = main_program.get_input("Please enter genre: rock ")
-print(get_last_oldest_of_genre(albums, genre))  ???? zobaczcie prosze czy errory wam wywala"""
-
-def get_longest_album(albums):
-
-#    Get album with biggest value in length field
-
-    biggest_length = albums[0][4].replace(":", ".")
-    longest_album = albums[0]
-
-    for i in range(len(albums)-1):
-        if float(biggest_length) < float(albums[i+1][4].replace(":", ".")):
-            biggest_length = albums[i+1][4].replace(":", ".")
-            longest_album = albums[i+1]
-
-    return longest_album
-
-def get_total_albums_length(albums):
-    """
-    Get sum of lengths of all albums in minutes, rounded to 2 decimal places
-    """
-    minutes = []
-    secound = []
-    time = []
-    for i in range(len(albums)):
-        time = albums[i][4].split(":")
-        minutes.append(int(time[0]))
-        secound.append(int(time[1]))
-
-    albums_length = (sum(minutes)*60 + sum(secound))/60
-
-    return round(albums_length, 2)
+album_list = [['Pink Floyd', 'The Dark Side Of The Moon', '1973', 'progressive rock', '43:00'], ['Britney Spears', 'Baby One More Time', '1999', 'pop', '42:20'], ['The Beatles', 'Revolver', '1966', 'rock', '34:43'], ['Deep Purple', 'Machine Head', '1972', 'hard rock', '37:25'], ['Old Timers', 'Old Time', '966', 'ancient', '123:45'], ['Pink Floyd', 'Wish You Were Here', '1975', 'progressive rock', '44:28'], ['Boston', 'Boston', '1976', 'hard rock', '37:41'], ['Monika Brodka', 'Granada', '2010', 'pop', '37:42'], ['David Bowie', 'Low', '1977', 'rock', '38:26'], ['rock', 'rock', '966', 'pop', '13:37'], ['Massive Attack', 'Blue Lines', '1991', 'hip hop', '45:02']]
+#print(count_genres(album_list))
+display_raport(album_list)
